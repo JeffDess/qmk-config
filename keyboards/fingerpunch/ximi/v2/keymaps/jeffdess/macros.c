8b772ecbe6e1d8e8f697d54e104a1400d9e7b130
Jean-François Dessureault <jfdess@gmail.com>
2024-10-24 08:34:43 -0400

    fix(ximi): working accent layer

--- keyboards/fingerpunch/ximi/v2/keymaps/jeffdess/macros.c
+++ keyboards/fingerpunch/ximi/v2/keymaps/jeffdess/macros.c
@@ -1,20 +1,28 @@
 #include <stdint.h>
 #include "sendstring_canadian_multilingual.h"
 
+#define SS_CIRC SS_TAP(X_LBRC)
+#define SS_TREM SS_ALGR(SS_TAP(X_LBRC))
+
 uint8_t mod_state;
 enum custom_keycodes {
     A_CI = SAFE_RANGE,
+    A_GR,
     E_CI,
+    E_CU,
+    E_GR,
     E_TR,
     I_CI,
     I_TR,
     O_CI,
     O_TR,
     U_CI,
+    U_GR,
     U_TR,
     GRAVE,
     TILDE,
     CIRC,
+    CEDIL,
     SLSH_BSLSH,
     DASH_TILDE,
     COMMA_DOT,
@@ -28,15 +36,26 @@ enum custom_keycodes {
 
 void deactivate_current_layer(void) {
     uint8_t current_layer = get_highest_layer(layer_state);
+    clear_keyboard_but_mods();
     layer_invert(current_layer);
 }
 
-void send_accent(const char *lower, const char *upper, uint8_t mod_state) {
+void send_circ_accent(const uint16_t letter, uint8_t mod_state) {
     if (mod_state & MOD_MASK_SHIFT) {
         del_mods(MOD_MASK_SHIFT);
-        SEND_STRING(upper);
+        SEND_STRING(SS_TAP(X_LBRC) SS_LSFT(SS_TAP(X_A)));
     } else {
-        SEND_STRING(lower);
+        SEND_STRING(SS_TAP(X_LBRC) SS_TAP(X_A));
+    }
+    deactivate_current_layer();
+}
+
+void send_diae_accent(const uint16_t letter, uint8_t mod_state) {
+    if (mod_state & MOD_MASK_SHIFT) {
+        del_mods(MOD_MASK_SHIFT);
+        SEND_STRING(SS_ALGR(SS_TAP(X_LBRC)) SS_LSFT(SS_TAP(X_A)));
+    } else {
+        SEND_STRING(SS_ALGR(SS_TAP(X_LBRC)) SS_TAP(X_A));
     }
     deactivate_current_layer();
 }
@@ -60,31 +79,105 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 SEND_STRING("~");
                 break;
             case A_CI:
-                send_accent("â", "Â", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_CIRC SS_LSFT(SS_TAP(X_A)));
+                } else {
+                    SEND_STRING(SS_CIRC SS_TAP(X_A));
+                }
+                deactivate_current_layer();
+                break;
+            case A_GR:
+                tap_code(CA_AGRV);
+                deactivate_current_layer();
+                break;
+            case CEDIL:
+                tap_code(CA_CCED);
+                deactivate_current_layer();
+                break;
+            case E_CU:
+                tap_code(CA_EACU);
+                deactivate_current_layer();
                 break;
             case E_CI:
-                send_accent("ê", "Ê", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_CIRC SS_LSFT(SS_TAP(X_E)));
+                } else {
+                    SEND_STRING(SS_CIRC SS_TAP(X_E));
+                }
+                deactivate_current_layer();
+                break;
+            case E_GR:
+                tap_code(CA_EGRV);
+                deactivate_current_layer();
                 break;
             case E_TR:
-                send_accent("ë", "Ë", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_TREM SS_LSFT(SS_TAP(X_E)));
+                } else {
+                    SEND_STRING(SS_TREM SS_TAP(X_E));
+                }
+                deactivate_current_layer();
                 break;
             case I_CI:
-                send_accent("î", "Î", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_CIRC SS_LSFT(SS_TAP(X_I)));
+                } else {
+                    SEND_STRING(SS_CIRC SS_TAP(X_I));
+                }
+                deactivate_current_layer();
                 break;
             case I_TR:
-                send_accent("ï", "Ï", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_TREM SS_LSFT(SS_TAP(X_I)));
+                } else {
+                    SEND_STRING(SS_TREM SS_TAP(X_I));
+                }
+                deactivate_current_layer();
                 break;
             case O_CI:
-                send_accent("ô", "Ô", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_CIRC SS_LSFT(SS_TAP(X_O)));
+                } else {
+                    SEND_STRING(SS_CIRC SS_TAP(X_O));
+                }
+                deactivate_current_layer();
                 break;
             case O_TR:
-                send_accent("ö", "Ö", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_TREM SS_LSFT(SS_TAP(X_O)));
+                } else {
+                    SEND_STRING(SS_TREM SS_TAP(X_O));
+                }
+                deactivate_current_layer();
                 break;
             case U_CI:
-                send_accent("û", "Û", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_CIRC SS_LSFT(SS_TAP(X_U)));
+                } else {
+                    SEND_STRING(SS_CIRC SS_TAP(X_U));
+                }
+                deactivate_current_layer();
+                break;
+            case U_GR:
+                tap_code(CA_UGRV);
+                deactivate_current_layer();
                 break;
             case U_TR:
-                send_accent("ü", "Ü", mod_state);
+                if (mod_state & MOD_MASK_SHIFT) {
+                    del_mods(MOD_MASK_SHIFT);
+                    SEND_STRING(SS_TREM SS_LSFT(SS_TAP(X_U)));
+                } else {
+                    SEND_STRING(SS_TREM SS_TAP(X_U));
+                }
+                deactivate_current_layer();
                 break;
             case FAT_ARROW:
                 SEND_STRING(" => ");
