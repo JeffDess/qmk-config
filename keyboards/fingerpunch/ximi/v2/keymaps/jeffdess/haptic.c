9792e72b045603f31c7fa0dce1dad79c79cd6ae4
Jean-François Dessureault <jfdess@gmail.com>
2024-11-01 21:06:18 -0400

    fix(ximi): undef DRV_2605L before redefining

--- keyboards/fingerpunch/ximi/v2/keymaps/jeffdess/haptic.c
+++ keyboards/fingerpunch/ximi/v2/keymaps/jeffdess/haptic.c
@@ -2,7 +2,9 @@
 #include "drivers/haptic/drv2605l.h"
 
 #ifdef HAPTIC_ENABLE
+#    undef DRV2605L_GREETING
 #    define DRV2605L_GREETING DRV2605L_EFFECT_TRANSITION_RAMP_UP_LONG_SMOOTH_2_50
+#    undef DRV2605L_DEFAULT_MODE
 #    define DRV2605L_DEFAULT_MODE DRV2605L_EFFECT_SHARP_TICK_1_100
 
 extern uint8_t split_haptic_play;
