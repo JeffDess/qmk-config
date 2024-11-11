d9b3fa3ebeaf008334ec9397d3865cecfcde2edc
Jean-François Dessureault <jfdess@gmail.com>
2024-11-10 20:13:22 -0500

    fix: ctrl-click shift-click not correctly configured

--- users/jeffdess/overrides.c
+++ users/jeffdess/overrides.c
@@ -13,17 +13,17 @@ const key_override_t comma_dot_override =
 const key_override_t slash_backslash_override =
     ko_make_with_layers(MOD_MASK_SHIFT, CA_SLSH, CA_BSLS, 1 << _SYMBOLS);
 
-const key_override_t escape_shift_click_override =
-    ko_make_basic(MOD_MASK_SHIFT, KC_ESC, S(KC_MS_BTN1));
-
 const key_override_t question_mark_inverted_override =
     ko_make_basic(MOD_MASK_SHIFT, CA_QUES, CA_IQUE);
 
 const key_override_t exclamation_inverted_override =
     ko_make_basic(MOD_MASK_SHIFT, CA_EXLM, CA_IEXL);
 
+const key_override_t escape_shift_click_override =
+    ko_make_basic(MOD_MASK_SHIFT, NUM_ESC, S(KC_MS_BTN1));
+
 const key_override_t escape_ctrl_click_override =
-    ko_make_basic(MOD_MASK_CTRL, KC_ESC, C(KC_MS_BTN1));
+    ko_make_basic(MOD_MASK_CTRL, NUM_ESC, C(KC_MS_BTN1));
 
 // clang-format off
 const key_override_t **key_overrides = (const key_override_t *[]){
@@ -33,6 +33,7 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &comma_dot_override,
     &slash_backslash_override,
     &escape_shift_click_override,
+    &escape_ctrl_click_override,
     &exclamation_inverted_override,
     &question_mark_inverted_override
 };
