772d7d8452774f8db1d3bb5fafe4f7f00600df45
Jean-François Dessureault <jfdess@gmail.com>
2024-11-16 11:41:39 -0500

    chore(ergolite): move config to default keymap

---
+++ keyboards/keyclicks/w_ergolite/keymaps/default/rules.mk
@@ -0,0 +1,20 @@
+VIA_ENABLE = no
+VIAL_ENABLE = no
+VIAL_INSECURE = no
+VIALRGB_ENABLE = no
+LTO_ENABLE = yes        # Reduce firmware size
+
+UART_DRIVER_REQUIRED = yes
+
+# COMMAND_ENABLE = yes  # Commands for debug and configuration
+# CONSOLE_ENABLE = yes  # Console for debug
+
+UNICODE_ENABLE = no
+OLED_DRIVER_ENABLE = no
+SPACE_CADET_ENABLE = no
+MUSIC_ENABLE = yes
+TAP_DANCE_ENABLE = no
+
+MIDI_ENABLE = yes
+BLUETOOTH_ENABLE = no
+# RGBLIGHT_ENABLE = no  # Enable WS2812 RGB underlight.
