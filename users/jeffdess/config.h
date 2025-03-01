999c5b2c8597d7db207ff9401e3138323eb6bce4
Jean-François Dessureault <jfdess@gmail.com>
2025-03-01 17:02:45 -0500

    feat: enable CHORDAL_HOLD

--- users/jeffdess/config.h
+++ users/jeffdess/config.h
@@ -5,11 +5,13 @@
 #endif
 
 // Timings
-#define TAPPING_TERM 220
+#define TAPPING_TERM 180
 #define TAPPING_TERM_PER_KEY
 #define THUMB_TAP_TERM 175
-#define HRM_TAP_TERM 280
-#define QUICK_TAP_TERM 175
+#define HRM_TAP_TERM 300
+#define QUICK_TAP_TERM 160
+#define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
 
 #ifdef ENCODER_ENABLE
 #    include "encoders.h"
