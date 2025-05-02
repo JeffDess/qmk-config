1b867a35916dfbabdd9d271f7d3128c6e6e4fe54
Jean-François Dessureault <jfdess@gmail.com>
2025-05-01 21:58:44 -0400

    feat: add keymap-drawer

--- flake.nix
+++ flake.nix
@@ -6,12 +6,27 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ qmk dfu-programmer dfu-util avrdude gcc ];
+        devShells.default = let keymap-drawer_version = "0.22.0";
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [
+            qmk
+            dfu-programmer
+            dfu-util
+            avrdude
+            gcc
+            python313
+            uv
+          ];
+          shellHook = ''
+            uv venv
+            source .venv/bin/activate
+            uv pip install keymap-drawer==${keymap-drawer_version}
+          '';
         };
+
       });
 }
