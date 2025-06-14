01f86d0e562d582e70e07e3277b6e7c1b9f23187
Jean-François Dessureault <jfdess@gmail.com>
2025-06-13 21:53:16 -0400

    feat: tweak TERM timings

--- users/jeffdess/config.h
+++ users/jeffdess/config.h
@@ -5,10 +5,10 @@
 #endif
 
 // Timings
-#define TAPPING_TERM 180
+#define TAPPING_TERM 200
 #define TAPPING_TERM_PER_KEY
-#define THUMB_TAP_TERM 175
-#define HRM_TAP_TERM 300
+#define THUMB_TAP_TERM 180
+#define HRM_TAP_TERM 220
 #define QUICK_TAP_TERM 160
 #define PERMISSIVE_HOLD
 #define CHORDAL_HOLD
