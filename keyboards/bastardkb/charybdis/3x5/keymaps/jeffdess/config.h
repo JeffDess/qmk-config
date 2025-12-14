24016f4e939214c62017eaead25acab53a656613
Jean-François Dessureault <jfdess@gmail.com>
2025-12-14 11:45:21 -0500

    fix(cnano): invert Y scroll

--- keyboards/bastardkb/charybdis/3x5/keymaps/jeffdess/config.h
+++ keyboards/bastardkb/charybdis/3x5/keymaps/jeffdess/config.h
@@ -4,7 +4,7 @@
 
 // Boot mode if pressing down outer thumb key while connecting the keyboard
 #define BOOTMAGIC_ROW_RIGHT 4
-#define BOOTMAGIC_COLUMN_RIGHT 6
+#define BOOTMAGIC_COLUMN_RIGHT 0
 #define BOOTMAGIC_ROW_LEFT 0
 #define BOOTMAGIC_COLUMN_LEFT 6
 
@@ -12,3 +12,5 @@
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
 #define LAYER_STATE_8BIT
+
+#define CHARYBDIS_DRAGSCROLL_REVERSE_Y
